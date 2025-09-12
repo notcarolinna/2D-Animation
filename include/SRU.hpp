1b28a5b77c17bfa5f9010848386a5c70e3fac531
Carolina Nóbrega <notcarolinna@Gmail.com>
2025-09-12 12:51:22 -0300

    Remove BoundingBox struct from SRU.hpp
    Removed the BoundingBox struct and related comments from SRU.hpp.

--- include/SRU.hpp
+++ include/SRU.hpp
@@ -1,22 +1,14 @@
 #pragma once
 
-// Eu puxo a bounding box das entidades para calcular a bounding box do mundo
-// Então preciso declarar a classe BoundingBox aqui
-
-class Entity;
-
-struct BoundingBox {
-    float xMin, yMin, xMax, yMax;
-};
+// A bounding box da SRU é um valor estático definido (geralmente o tamanho da tela)
+// Então screenSize = 800x800
 
 class SRU {
     private:
     int scale;
-    BoundingBox worldBoundingBox;
 
     public: 
     SRU();
     void setScale(int s);
     int getScale() const;
-    BoundingBox getWorldBoundingBox() const;
 }; 
